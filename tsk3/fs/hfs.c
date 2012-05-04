87c347d58e479e0fd276779a7596d1453b887139
Rob Joyce <rob@redglow.org>
2012-05-04 11:32:35 -0400

    Fix our custom hfs_get_inode_name to work with new HFS function name.

--- tsk3/fs/hfs.c
+++ tsk3/fs/hfs.c
@@ -4497,9 +4497,9 @@ char *hfs_get_inode_name(TSK_FS_INFO * fs, TSK_INUM_T inum) {
 	if ( fn == NULL )
 		return NULL;
 	
-    if (hfs_uni2ascii(fs, entry.thread.name.unicode,
+    if (hfs_UTF16toUTF8(fs, entry.thread.name.unicode,
 					  tsk_getu16(fs->endian, entry.thread.name.length), fn,
-					  HFS_MAXNAMLEN + 1)) {
+					  HFS_MAXNAMLEN + 1, HFS_U16U8_FLAG_REPLACE_SLASH)) {
 		free(fn);
         return NULL;
 	}
